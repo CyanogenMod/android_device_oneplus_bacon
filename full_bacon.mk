1ec29013f6188ffc1dbbb9ebc5f17125bc7f2519
Peter Carenza <pcarenza@gmail.com>
2015-01-16 15:37:42 -0500

    Force LTE icon
    Change-Id: I71d96f03547edbda00dc5e30b540ef543ecc23ff

--- full_bacon.mk
+++ full_bacon.mk
@@ -14,60 +14,16 @@
 # limitations under the License.
 #
 
-# overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay vendor/extra/overlays/phone-1080p
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Haters gonna hate..
-PRODUCT_CHARACTERISTICS := nosdcard
+# Inherit from find7 device
+$(call inherit-product, device/oneplus/bacon/bacon.mk)
 
-# Ramdisk
-PRODUCT_PACKAGES += \
-libinit_bacon \
-fstab.bacon \
-init.bacon.rc \
-init.qcom.usb.rc \
-ueventd.bacon.rc
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := full_bacon
+PRODUCT_DEVICE := bacon
+PRODUCT_BRAND := oneplus
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := OnePlusOne
 
-# Audio
-PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-$(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
-
-# NFC packages
-PRODUCT_PACKAGES += \
-NfcNci \
-Tag \
-nfc_nci.pn54x.default \
-com.android.nfc_extras
-
-# NFC access control + feature files + configuration
-PRODUCT_COPY_FILES += \
-frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-$(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-$(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
-
-# System properties
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.telephony.default_network=20
-
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-# call dalvik heap config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-
-# call hwui memory config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
-# call the proprietary setup
-$(call inherit-product-if-exists, vendor/oneplus/bacon/bacon-vendor.mk)
-
-ifneq ($(QCPATH),)
-$(call inherit-product-if-exists, $(QCPATH)/prebuilt_HY11/target/product/msm8974/prebuilt.mk)
-endif
-
-# Inherit from msm8974-common
-$(call inherit-product, device/oppo/msm8974-common/msm8974.mk)
