f455c9a02d93e57296492c718ed9dbfb32d546e2
Sultanxda <sultanxda@gmail.com>
2015-05-28 18:00:14 -0700

    bacon: power: Remove unneeded/incompatible features
    *Updated maxfreq for bacon

--- power/power_bacon.c
+++ power/power_bacon.c
@@ -35,34 +35,16 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define STATE_ON "state=1"
-#define STATE_OFF "state=0"
-#define STATE_HDR_ON "state=2"
-#define STATE_HDR_OFF "state=3"
-
-#define MAX_LENGTH         50
-#define BOOST_SOCKET       "/dev/socket/pb"
-
-#define UEVENT_MSG_LEN 2048
-#define TOTAL_CPUS 4
-#define RETRY_TIME_CHANGING_FREQ 20
-#define SLEEP_USEC_BETWN_RETRY 200
-#define LOW_POWER_MAX_FREQ "729600"
-#define LOW_POWER_MIN_FREQ "300000"
-#define NORMAL_MAX_FREQ "2265600"
-#define UEVENT_STRING "online@/devices/system/cpu/"
-
-static int client_sockfd;
-static struct sockaddr_un client_addr;
-static int last_state = -1;
+#define MAX_LENGTH			50
+#define UEVENT_MSG_LEN			2048
+#define TOTAL_CPUS			4
+#define RETRY_TIME_CHANGING_FREQ	20
+#define SLEEP_USEC_BETWN_RETRY		200
+#define LOW_POWER_MAX_FREQ		"729600"
+#define NORMAL_MAX_FREQ			"2457600"
+#define UEVENT_STRING			"online@/devices/system/cpu/"
 
 static struct pollfd pfd;
-static char *cpu_path_min[] = {
-    "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq",
-    "/sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq",
-    "/sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq",
-    "/sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq",
-};
 static char *cpu_path_max[] = {
     "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
     "/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq",
@@ -72,20 +54,6 @@ static char *cpu_path_max[] = {
 static bool freq_set[TOTAL_CPUS];
 static bool low_power_mode = false;
 static pthread_mutex_t low_power_mode_lock = PTHREAD_MUTEX_INITIALIZER;
-
-static void socket_init()
-{
-    if (!client_sockfd) {
-        client_sockfd = socket(PF_UNIX, SOCK_DGRAM, 0);
-        if (client_sockfd < 0) {
-            ALOGE("%s: failed to open: %s", __func__, strerror(errno));
-            return;
-        }
-        memset(&client_addr, 0, sizeof(struct sockaddr_un));
-        client_addr.sun_family = AF_UNIX;
-        snprintf(client_addr.sun_path, UNIX_PATH_MAX, BOOST_SOCKET);
-    }
-}
 
 static int sysfs_write(const char *path, char *s)
 {
@@ -138,7 +106,6 @@ static int uevent_event()
         pthread_mutex_lock(&low_power_mode_lock);
         if (low_power_mode && !freq_set[cpu]) {
             while (retry) {
-                sysfs_write(cpu_path_min[cpu], LOW_POWER_MIN_FREQ);
                 ret = sysfs_write(cpu_path_max[cpu], LOW_POWER_MAX_FREQ);
                 if (!ret) {
                     freq_set[cpu] = true;
@@ -206,136 +173,12 @@ static void uevent_init()
 static void power_init(__attribute__((unused)) struct power_module *module)
 {
     ALOGI("%s", __func__);
-    socket_init();
     uevent_init();
 }
 
-static void sync_thread(int off)
+static void power_set_interactive(__attribute__((unused)) struct power_module *module, 
+                      __attribute__((unused)) int on)
 {
-    int rc;
-    pid_t client;
-    char data[MAX_LENGTH];
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    client = getpid();
-
-    if (!off) {
-        snprintf(data, MAX_LENGTH, "2:%d", client);
-        rc = sendto(client_sockfd, data, strlen(data), 0,
-            (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    } else {
-        snprintf(data, MAX_LENGTH, "3:%d", client);
-        rc = sendto(client_sockfd, data, strlen(data), 0,
-            (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    }
-
-    if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
-    }
-}
-
-static void enc_boost(int off)
-{
-    int rc;
-    pid_t client;
-    char data[MAX_LENGTH];
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    client = getpid();
-
-    if (!off) {
-        snprintf(data, MAX_LENGTH, "5:%d", client);
-        rc = sendto(client_sockfd, data, strlen(data), 0,
-            (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    } else {
-        snprintf(data, MAX_LENGTH, "6:%d", client);
-        rc = sendto(client_sockfd, data, strlen(data), 0,
-            (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    }
-
-    if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
-    }
-}
-
-static void process_video_encode_hint(void *metadata)
-{
-
-    socket_init();
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    if (metadata) {
-        if (!strncmp(metadata, STATE_ON, sizeof(STATE_ON))) {
-            /* Video encode started */
-            sync_thread(1);
-            enc_boost(1);
-        } else if (!strncmp(metadata, STATE_OFF, sizeof(STATE_OFF))) {
-            /* Video encode stopped */
-            sync_thread(0);
-            enc_boost(0);
-        }  else if (!strncmp(metadata, STATE_HDR_ON, sizeof(STATE_HDR_ON))) {
-            /* HDR usecase started */
-        } else if (!strncmp(metadata, STATE_HDR_OFF, sizeof(STATE_HDR_OFF))) {
-            /* HDR usecase stopped */
-        }else
-            return;
-    } else {
-        return;
-    }
-}
-
-
-static void touch_boost()
-{
-    int rc;
-    pid_t client;
-    char data[MAX_LENGTH];
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    client = getpid();
-
-    snprintf(data, MAX_LENGTH, "1:%d", client);
-    rc = sendto(client_sockfd, data, strlen(data), 0,
-        (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
-    }
-}
-
-static void power_set_interactive(__attribute__((unused)) struct power_module *module, int on)
-{
-    if (last_state == -1) {
-        last_state = on;
-    } else {
-        if (last_state == on)
-            return;
-        else
-            last_state = on;
-    }
-
-    ALOGV("%s %s", __func__, (on ? "ON" : "OFF"));
-    if (on) {
-        sync_thread(0);
-        touch_boost();
-    } else {
-        sync_thread(1);
-    }
 }
 
 static void power_hint( __attribute__((unused)) struct power_module *module,
@@ -343,45 +186,28 @@ static void power_hint( __attribute__((unused)) struct power_module *module,
 {
     int cpu, ret;
 
-    switch (hint) {
-        case POWER_HINT_INTERACTION:
-            ALOGV("POWER_HINT_INTERACTION");
-            touch_boost();
-            break;
-#if 0
-        case POWER_HINT_VSYNC:
-            ALOGV("POWER_HINT_VSYNC %s", (data ? "ON" : "OFF"));
-            break;
-#endif
-        case POWER_HINT_VIDEO_ENCODE:
-            process_video_encode_hint(data);
-            break;
+    if (hint != POWER_HINT_LOW_POWER)
+        return;
 
-        case POWER_HINT_LOW_POWER:
-             pthread_mutex_lock(&low_power_mode_lock);
-             if (data) {
-                 low_power_mode = true;
-                 for (cpu = 0; cpu < TOTAL_CPUS; cpu++) {
-                     sysfs_write(cpu_path_min[cpu], LOW_POWER_MIN_FREQ);
-                     ret = sysfs_write(cpu_path_max[cpu], LOW_POWER_MAX_FREQ);
-                     if (!ret) {
-                         freq_set[cpu] = true;
-                     }
-                 }
-             } else {
-                 low_power_mode = false;
-                 for (cpu = 0; cpu < TOTAL_CPUS; cpu++) {
-                     ret = sysfs_write(cpu_path_max[cpu], NORMAL_MAX_FREQ);
-                     if (!ret) {
-                         freq_set[cpu] = false;
-                     }
-                 }
-             }
-             pthread_mutex_unlock(&low_power_mode_lock);
-             break;
-        default:
-             break;
+    pthread_mutex_lock(&low_power_mode_lock);
+    if (data) {
+        low_power_mode = true;
+        for (cpu = 0; cpu < TOTAL_CPUS; cpu++) {
+            ret = sysfs_write(cpu_path_max[cpu], LOW_POWER_MAX_FREQ);
+            if (!ret) {
+                freq_set[cpu] = true;
+            }
+        }
+    } else {
+        low_power_mode = false;
+        for (cpu = 0; cpu < TOTAL_CPUS; cpu++) {
+            ret = sysfs_write(cpu_path_max[cpu], NORMAL_MAX_FREQ);
+            if (!ret) {
+                freq_set[cpu] = false;
+            }
+        }
     }
+    pthread_mutex_unlock(&low_power_mode_lock);
 }
 
 static struct hw_module_methods_t power_module_methods = {
@@ -394,11 +220,10 @@ struct power_module HAL_MODULE_INFO_SYM = {
         .module_api_version = POWER_MODULE_API_VERSION_0_2,
         .hal_api_version = HARDWARE_HAL_API_VERSION,
         .id = POWER_HARDWARE_MODULE_ID,
-        .name = "Hammerhead Power HAL",
-        .author = "The Android Open Source Project",
+        .name = "Bacon Power HAL",
+        .author = "The CyanogenMod Project",
         .methods = &power_module_methods,
     },
-
     .init = power_init,
     .setInteractive = power_set_interactive,
     .powerHint = power_hint,
