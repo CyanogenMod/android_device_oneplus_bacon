f33e1e417a782b7038d05685d5911cafa8cca69f
Varun Date <date.varun123@gmail.com>
2016-03-14 20:11:44 +0530

    Boardconfig:Bring It Inline With CM
    --No more Risks
    
    Change-Id: Iec4ba213c1db1770b200c79dba381ae473d8ea4c
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,14 +46,15 @@ TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := resurrected_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
@@ -194,6 +195,3 @@ endif
 endif
 
 -include vendor/oneplus/bacon/BoardConfigVendor.mk
-
-TARGET_GCC_VERSION_EXP:=4.9
-KERNEL_TOOLCHAIN := /home/varun/RR/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin
