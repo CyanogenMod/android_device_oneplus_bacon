ec70c9bbbfb03056cee12f9471516d316de3f470
Jithu Mathew Joseph <jithu311990@gmail.com>
2016-06-13 15:41:26 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_bacon-user
-add_lunch_combo cm_bacon-userdebug
-add_lunch_combo cm_bacon-eng
+add_lunch_combo xosp_bacon-user
+add_lunch_combo xosp_bacon-userdebug
+add_lunch_combo xosp_bacon-eng
