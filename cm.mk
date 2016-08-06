c8e70f41cd3b38dbe1e03112208ef3bcab309d02
Nuno Silva <nafsilva@gmail.com>
2016-08-06 12:04:14 +0100

    Add Build Tag "temasek"

--- cm.mk
+++ cm.mk
@@ -24,6 +24,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Custom unofficial build tag
+TARGET_UNOFFICIAL_BUILD_ID := temasek
+
 PRODUCT_NAME := cm_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
